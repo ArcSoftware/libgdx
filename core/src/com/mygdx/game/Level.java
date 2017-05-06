b58f9fce724088793207a56faf16beeee3f8a30a
Jake T <jaketepper@gmail.com>
2017-05-06 18:00:41 -0400

    Added TMX file from PNG

--- core/src/com/mygdx/game/Level.java
+++ core/src/com/mygdx/game/Level.java
@@ -3,24 +3,27 @@ package com.mygdx.game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.maps.MapLayer;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import javafx.stage.Screen;
 
 /**
  * Created by Jake on 5/4/17.
  */
 public class Level {
     private Music music;
-    private Texture map;
+    private TiledMap map;
+    private MapLayer layer;
 
     public Level(String textureFileName, String musicFileName) {
-        map = new Texture(textureFileName);
+        map = new TmxMapLoader().load(textureFileName);
         music = Gdx.audio.newMusic(Gdx.files.internal(musicFileName));
+        layer = map.getLayers().get(0);
     }
 
-    public Music getMusic() {
-        return music;
-    }
-
-    public Texture getMap() {
+    public TiledMap getMap() {
         return map;
     }
 
@@ -32,4 +35,10 @@ public class Level {
         music.play();
     }
 
+    public MapLayer getLayer() {return layer;}
+
+    //    @Override
+//    public void render(float delta) {
+//
+//    }
 }
--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -5,7 +5,12 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.GL20;
 
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.IsometricTiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -22,31 +27,49 @@ public class MyGdxGame extends ApplicationAdapter {
 	final int HEIGHT = 17;
 	final int DRAW_WIDTH = WIDTH * 3;
 	final int DRAW_HEIGHT = HEIGHT * 3;
-
+	TmxMapLoader mapLoader;
+    float unitScale = 1 / 32f;
+    OrthogonalTiledMapRenderer renderer;
+    OrthographicCamera camera;
 	@Override
 	public void create() {
 		levels = new HashMap<>();
-		levels.put("ironforge", new Level("ironforge.png", "ironforge.mp3"));
+		levels.put("ironforge", new Level("ironForge.tmx", "ironforge.mp3"));
 		currentLevel = levels.get("ironforge");
 		batch = new SpriteBatch();
 		p = new Player();
 		hogger = new Object();
 		flag = new Object();
 		playerMovement = new Movement();
-	}
+
+		mapLoader = new TmxMapLoader();
+        renderer = new OrthogonalTiledMapRenderer(currentLevel.getMap(), unitScale);
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false,27, 21);
+        renderer.setView(camera);
+
+
+    }
 
 	@Override
 	public void render() {
 		time += Gdx.graphics.getDeltaTime();
-		playerMovement.moveMainPlayer(p);
 		background();
+		renderer.render();
 
 		batch.begin();
-		batch.draw(currentLevel.getMap(), -120, -100, 1340, 870);
-		batch.draw(flag.getFlag(), 10, 300, 150, 160);
+		playerMovement.moveMainPlayer(p);
 		batch.draw(p.getCurrentTexture(), p.x, p.y, DRAW_WIDTH, DRAW_HEIGHT);
+
+
+
+		batch.draw(flag.getFlag(), 10, 300, 150, 160);
+
 		batch.draw(hogger.getHogger(), 200, 120, 150, 100);
 //		batch.draw(w.getWall(), 100, 100, 40, 40);
+//        currentLevel.getMap();
+//        currentLevel.getLayer();
+
 		batch.end();
 
 	}
@@ -54,6 +77,8 @@ public class MyGdxGame extends ApplicationAdapter {
 	@Override
 	public void dispose() {
 		batch.dispose();
+		renderer.dispose();
+
 //		sound.dispose();
 
 	}
